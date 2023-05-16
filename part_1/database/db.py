70da4cf64f4c3f643ae102d27ffbf054d326011f
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-05-16 21:05:24 +0300

    draft 6

--- part_1/database/db.py
+++ part_1/database/db.py
@@ -4,20 +4,11 @@ import pathlib
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from conf.config import settings
 
-file_config = pathlib.Path(__file__).parent.joinpath('config.ini')
-config = configparser.ConfigParser()
-config.read(file_config)
+SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 
-username = config.get("DEV_DB", "USER")
-password = config.get("DEV_DB", "PASSWORD")
-domain = config.get("DEV_DB", "DOMAIN")
-port = config.get("DEV_DB", "PORT")
-database = config.get("DEV_DB", "DB_NAME")
-
-url = f"postgresql://{username}:{password}@{domain}:{port}/{database}"
-
-engine = create_engine(url, echo = True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo = True)
 DBSession = sessionmaker(autocommit= False, autoflush=False, bind=engine)
 
 Base = declarative_base()
